74711adc890ab413232644ec704bff12afc756d6
Dragan Pavličević <dragan@smartweb.rs>
2025-01-02 18:35:46 +0100

    center zoomed position

--- src/App.js
+++ src/App.js
@@ -525,7 +525,7 @@ function MobileL({windowSize, device}) {
 
 
 
-        const newPosX = (clickX * scaleX)/2.2;
+        const newPosX = (clickX * scaleX)/2.4;
         const newPosY = (clickY * scaleY)/1.2;
 
         console.log(e);
